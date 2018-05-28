88cbb2524a6c83e1005bca1d6c5db4317ed4e569
Bill Thornton <bill.thornton@yahoo.com>
2018-05-28 14:20:13 -0400

    Adding in the script for the data loading

--- plot3.R
+++ plot3.R
@@ -1,3 +1,14 @@
+# Load in the lines from the text file
+# NOTE -- We skip to the first line with the correct data (1/2/2007)
+#         and we only load in the rows (2,880) for the two dates
+pwr <- read.table('../household_power_consumption.txt', header = FALSE, sep = ";", skip = 66637, nrows = 2880)
+
+# Skipping the lines stripped the header.  Adding back in
+colnames(pwr) = c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+# Creating new field, DateTime by merging the Date and Time fields and formatting correctly
+pwr$DateTime <- strptime(paste(pwr$Date,pwr$Time), "%d/%m/%Y %H:%M")
+
 # Set the graphic device
 png(file = "plot3.png")
 
